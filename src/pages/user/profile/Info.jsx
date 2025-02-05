5efa763b6b5a471b835c2c19f7b742da781176a8
hamada <bhak6150@gmail.com>
2025-02-05 22:11:50 +0100

    Info - UserContext

--- src/pages/user/profile/Info.jsx
+++ src/pages/user/profile/Info.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { useContext } from 'react';
+import { UserContext } from '../../../components/user/context/UserContext';
 
 export default function Info() {
+
+    const {user , loading } = useContext(UserContext);
   return (
-    <div>info</div>
+    <>
+    <h1>Info Page</h1>
+    <h1> User Name : {user.userName}</h1>
+    <h1> User Email : {user.email}</h1>
+    
+
+
+    {loading && <p>Loading...</p>}
+
+
+   
+
+
+
+    
+    </>
   )
 }
